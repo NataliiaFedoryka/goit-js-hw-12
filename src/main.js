393f3030a9e0d9ebcaa449cb6272444e91283749
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-06-24 19:45:51 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 
-// axios({
-//   method: 'get',
-//   url: 'https://jsonplaceholder.typicode.com/users'
-// })
-// 	.then(response => console.log(response))
-// 	.catch(error => console.log(error));
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 

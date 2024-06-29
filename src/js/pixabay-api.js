0299a19f79a6458e78855a79aa12eb6abbb2a96e
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-06-29 10:25:33 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 
-import axios from `axios`;
-const API_KEY = `44528758-32c26e02cbc7bd56fd7d9b89c`;
-const BASE_URL = `https://pixabay.com/api/`;
+import axios from 'axios';
+const API_KEY = '44528758-32c26e02cbc7bd56fd7d9b89c';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1) {
     const params = {
